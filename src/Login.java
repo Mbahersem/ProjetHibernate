ef1e2d841f7278932a6c2c1a10089c91af57c48f
[MH] <[mbailassem1st@gmail.com]>
2023-04-29 14:38:36 +0000

    La page de connexion et sa partie FXML

---
+++ src/Login.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Login extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Connexion");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
