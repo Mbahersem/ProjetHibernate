2b8f968dc3a1f5c6c3e44627cd29f674727a8b82
[MH] <[mbailassem1st@gmail.com]>
2023-04-30 13:03:58 +0000

    Modification interface de l'application

--- src/code/AppController.java
+++ src/code/AppController.java
@@ -15,17 +15,32 @@ import org.hibernate.cfg.Configuration;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
 public class AppController {
     
+
+    @FXML
+    private Button btAjoutLiv;
+
     @FXML
     private Button btEnregistrerMembre;
 
     @FXML
     private Button btOngInsc;
+
+    @FXML
+    private Button btnActualiserEmp;
+
+    @FXML
+    private Button btnActualiserEmp1;
+
+    @FXML
+    private Button btnAjoutEmprunt;
 
     @FXML
     private Button butOngEmp;
@@ -35,6 +50,21 @@ public class AppController {
 
     @FXML
     private Button butOngMem;
+
+    @FXML
+    private TableColumn<?, ?> colCNIMem;
+
+    @FXML
+    private TableColumn<?, ?> colIdMem;
+
+    @FXML
+    private TableColumn<?, ?> colNomMem;
+
+    @FXML
+    private TableColumn<?, ?> colPrenomMem;
+
+    @FXML
+    private TableColumn<?, ?> colTelMem;
 
     @FXML
     private Label lblHead;
@@ -64,6 +94,15 @@ public class AppController {
     private Label tfCNI;
 
     @FXML
+    private TextField tfEmpLiv;
+
+    @FXML
+    private TextField tfEmpMem;
+
+    @FXML
+    private TextField tfInvLiv;
+
+    @FXML
     private Label tfNom;
 
     @FXML
@@ -72,6 +111,25 @@ public class AppController {
     @FXML
     private Label tfTel;
     
+    @FXML
+    void actualiserEmprunt(MouseEvent event) {
+
+    }
+
+    @FXML
+    void actualiserLivreTab(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ajouterEmprunt(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ajouterLivre(MouseEvent event) {
+
+    }
 
     @FXML
     void ongletEmprunts(MouseEvent event) {
